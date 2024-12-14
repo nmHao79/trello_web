4ee17ed688400a0f63c292656e44d0f5176366d5
nmhao <nmhao.git@gmail.com>
2024-12-14 18:26:43 +0700

    Fix BUILD-MODE dev and production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,12 @@
-export const API_ROOT = ['http://localhost:8017']
+/* eslint-disable no-undef */
+// export const API_ROOT = ['http://localhost:8017']
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-be.onrender.com'
+}
 //call backend
+export const API_ROOT = apiRoot
+console.log("🚀 ~ apiRoot:", apiRoot)
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
+  //Cho phep thang vite su dung dc process.env
+  define: {
+    'process.env' : process.env
+  },
   plugins: [
     react(),
     svgr()
